0a1f1419366ee61e768dfa7648f0a7b8a0518958
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-01-23 20:48:15 +0200

    Initial Commit

---
+++ BeardGramUITests/BeardGramUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BeardGramUITestsLaunchTests.swift
+//  BeardGramUITests
+//
+//  Created by Pavel Semenchenko on 23.01.2023.
+//
+
+import XCTest
+
+final class BeardGramUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
