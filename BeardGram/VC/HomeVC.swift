3cf6eed86b9fba0c1428e75fa3e851f187476278
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-02-01 15:22:29 +0200

    add logOut func

--- BeardGram/VC/HomeVC.swift
+++ BeardGram/VC/HomeVC.swift
@@ -11,17 +11,31 @@ import UIKit
 class HomeVC: UIViewController {
     
     @IBOutlet weak var contactsTableView: UITableView!
-    
+    let authenticationService: AuthenticationService = FirebaseAuthenticationService()
     let contactRepository = ContactsRepository()
     var contacts: [Contact] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    title = "Contacts list"
+        title = "Contacts list"
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(title: "LogOut",
+                                                              style: UIBarButtonItem.Style.plain,
+                                                              target: self,
+                                                              action: #selector(logOutClicked))]
         
         let cellNib = UINib(nibName: "ContactTableViewCell", bundle: nil)
     }
-    
+    @objc func logOutClicked() {
+        authenticationService.logOut()
+        if navigationController?.viewControllers.count == 1 {
+            guard let viewController = storyboard?.instantiateViewController(withIdentifier: "signInSB") else {
+                return
+            }
+            navigationController?.pushViewController(viewController, animated: true)
+        } else {
+            navigationController?.popToRootViewController(animated: true)
+        }
+    }
     
     
 }
