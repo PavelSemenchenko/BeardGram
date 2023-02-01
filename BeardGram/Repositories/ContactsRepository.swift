ba6a5ed86b8f92b629f030c9d3ccc47884e7c5aa
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-02-01 22:55:49 +0200

    edit repository of contacts

--- BeardGram/Repositories/ContactsRepository.swift
+++ BeardGram/Repositories/ContactsRepository.swift
@@ -6,19 +6,52 @@
 //
 
 import Foundation
-import Alamofire
+// import Alamofire
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
-struct Contact: Codable {
-    let id: Int
+struct ContactProfile: Codable {
+    @DocumentID var id: String?
     let name: String
     let email: String
 }
 
-class ContactsRepository {
-    func loadAll(completion: @escaping ([Contact]) -> Void) {
-        let request = AF.request("link to load from web")
-        request.responseDecodable(of: [Contact].self) { contactsResponse in
-            completion(contactsResponse.value ?? [])
+protocol ContactProfilesRepository {
+    func getAll()
+    func getOne(userId: String, completion: @escaping (ContactProfile?) -> Void)
+    func create(profile: ContactProfile)
+    func delete()
+}
+
+class FirebaseContactProfilesRepository: ContactProfilesRepository {
+    func getAll() {
+        <#code#>
+    }
+    
+    func getOne(userId: String, completion: @escaping (ContactProfile?) -> Void) {
+        Firestore.firestore().collection("contacts").document(userId).getDocument(as: ContactProfile.self) { snapshot in
+            switch(snapshot) {
+            case .success(let contact):
+                completion(contact)
+            case .failure(_):
+                completion(nil)
+            }
         }
     }
+    
+    func create(profile: ContactProfile) {
+        var contactProfile = profile
+        guard let currentUserId = Auth.auth().currentUser?.uid else {
+            fatalError("Only authenticated users")
+        }
+        contactProfile.id = currentUserId
+        try? Firestore.firestore().collection("contacts").document(currentUserId).setData(from: contactProfile)
+    }
+    
+    func delete() {
+        <#code#>
+    }
+    
+    
 }
