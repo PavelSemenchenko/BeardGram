3867a4022d45fff3f2221835d01e11cc09d284cb
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-02-06 21:21:14 +0200

    PBG-012 add UI screens

---
+++ BeardGram/VC/GlobalSearchVC.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalSearchVC.swift
+//  BeardGram
+//
+//  Created by Pavel Semenchenko on 06.02.2023.
+//
+
+import Foundation
+import UIKit
+
+class GlobalSearchVC: UIViewController {
+    
+}
