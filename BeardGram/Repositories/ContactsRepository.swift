811dd874eb86f65a1a0bc654c3c5b2d15a369c53
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-02-10 22:36:14 +0200

    add listener fix delete contact

--- BeardGram/Repositories/ContactsRepository.swift
+++ BeardGram/Repositories/ContactsRepository.swift
@@ -41,7 +41,7 @@ class FirebaseContactsRepository: ContactsRepository {
         guard let currentUserId = Auth.auth().currentUser?.uid else {
             fatalError("no permissions")
         }
-        contactsCollection.document(currentUserId).collection("contacts").getDocuments { snapshot, _ in
+        contactsCollection.document(currentUserId).collection("contacts").addSnapshotListener { snapshot, _ in
             guard let docs = snapshot?.documents else {
                 completion([])
                 return
